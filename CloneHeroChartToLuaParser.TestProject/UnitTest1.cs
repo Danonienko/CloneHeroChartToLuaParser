75fbf115fcde0b91f9602278b3c50369249fd75f
Danonienko <Danonienko8@gmail.com>
2026-03-17 13:45:00 +0200

    chore: Add unit test logic

--- CloneHeroChartToLuaParser.TestProject/UnitTest1.cs
+++ CloneHeroChartToLuaParser.TestProject/UnitTest1.cs
@@ -16,11 +16,97 @@ namespace CloneHeroChartToLuaParser.TestProject
         }
 
         [Test]
-        public void SectionsShouldNotBeEmpty()
+        public void SectionsShouldBeParsedCorrectly()
         {
             var sections = Parser.ReadSections(fileContent);
 
-            sections.Should().NotBeEmpty();
+            sections.Should().NotBeNullOrEmpty();
+            sections.Should().HaveCount(15);
+        }
+
+        [Test]
+        public void SongSectionShouldBeParsedCorrectly()
+        {
+            var sections = Parser.ReadSections(fileContent);
+
+            var songSection = sections["[Song]"];
+            songSection.Should().NotBeNullOrEmpty();
+        }
+
+        [Test]
+        public void SongDataShouldBeParsedCorrectly()
+        {
+            var sections = Parser.ReadSections(fileContent);
+            var songSection = sections["[Song]"];
+
+            var songData = Parser.ReadSongData(songSection);
+            songData.Should().NotBeNull();
+            songData.Offset.Should().Be(0);
+            songData.Resolution.Should().Be(480);
+            songData.Player2.Should().Be("bass");
+            songData.Difficulty.Should().Be(0);
+            songData.PreviewStart.Should().Be(0);
+            songData.PreviewEnd.Should().Be(0);
+            songData.Genre.Should().Be("rock");
+            songData.MediaType.Should().Be("cd");
+        }
+
+        [Test]
+        public void SyncTracksSectionShouldBeParsedCorrectly()
+        {
+            var sections = Parser.ReadSections(fileContent);
+
+            var syncTracksSection = sections["[SyncTrack]"];
+            syncTracksSection.Should().NotBeNullOrEmpty();
+        }
+
+        [Test]
+        public void SyncTracksDataShouldBeParsedCorrectly()
+        {
+            var sections = Parser.ReadSections(fileContent);
+            var syncTracksSection = sections["[SyncTrack]"];
+
+            var syncTracksData = Parser.ReadSyncTracks(syncTracksSection);
+            syncTracksData.Should().NotBeNullOrEmpty();
+            syncTracksData.Should().HaveCount(81);
+
+            var syncTrack = syncTracksData[0];
+            syncTrack.Tick.Should().Be(0);
+            syncTrack.Type.Should().Be("TS");
+            syncTrack.Value.Should().Be(6);
+        }
+
+        [Test]
+        public void EventsSectionShouldBeParsedCorrectly()
+        {
+            var sections = Parser.ReadSections(fileContent);
+
+            var eventsSection = sections["[Events]"];
+            eventsSection.Should().NotBeNullOrEmpty();
+        }
+
+        [Test]
+        public void EventsDataShouldBeParsedCorrectly()
+        {
+            var sections = Parser.ReadSections(fileContent);
+            var eventsSection = sections["[Events]"];
+
+            var eventsData = Parser.ReadEvents(eventsSection);
+            eventsData.Should().NotBeNullOrEmpty();
+            eventsData.Should().HaveCount(140);
+
+            var eventData = eventsData[0];
+            eventData.Tick.Should().Be(5280);
+            eventData.Type.Should().Be("E");
+            eventData.Value.Should().Be("phrase_start");
+        }
+
+        [Test]
+        public void NotesSectionShouldBeParsedCorrectly()
+        {
+            var sections = Parser.ReadSections(fileContent);
+            var notesSection = sections["[ExpertSingle]"];
+            notesSection.Should().NotBeNullOrEmpty();
         }
     }
 }
